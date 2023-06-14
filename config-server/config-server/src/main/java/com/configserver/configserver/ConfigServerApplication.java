4d8322bc5ee769523ad257e1dd33db774ad2e81e
mohosinmiah <hamza1610330816@gmail.com>
2023-06-14 06:41:17 +0600

    Config Server added coofiguration properties

--- config-server/config-server/src/main/java/com/configserver/configserver/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/com/configserver/configserver/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
